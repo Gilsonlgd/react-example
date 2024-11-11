d4c2d3571b8df5cba9b3194b075119a4c2d7f959
Gilsonlgd <garciadasilvagilson@gmail.com>
2024-11-11 16:33:28 -0300

    ♻️ refactor: Small updates

--- src/router.tsx
+++ src/router.tsx
@@ -1,18 +1,24 @@
 import { createBrowserRouter } from 'react-router-dom';
 
-/**
- * import PublicLayout from '@layout/public/PublicLayout';
- * import publicRoutes from './routes/public';
- */
+import AdminLayout from '@layout/admin/AdminLayout';
+import PublicLayout from '@layout/public/PublicLayout';
+
+import adminRoutes from './routes/admin';
+import publicRoutes from './routes/public';
 
 const router = createBrowserRouter([
   {
     children: [
-      /* {
+      {
         path: '/',
         element: <PublicLayout />,
         children: publicRoutes,
-      }, */
+      },
+      {
+        path: '/admin',
+        element: <AdminLayout />,
+        children: adminRoutes,
+      },
     ],
   },
 ]);
