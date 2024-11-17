f4a89fb3d93423509c0bbf6d310445bc8db0f4ee
Gilsonlgd <garciadasilvagilson@gmail.com>
2024-11-17 19:13:28 -0300

    ♻️ refactor: Update router.tsx

--- src/router.tsx
+++ src/router.tsx
@@ -1,22 +1,25 @@
 import { Navigate, createBrowserRouter } from 'react-router-dom';
 
 import AdminLayout from '@layout/admin/AdminLayout';
-/* import WorkerLayout from '@layout/worker/WorkerLayout'; */
+import WorkerLayout from '@layout/worker/WorkerLayout';
 
 import adminRoutes from './routes/admin';
-/* import workerRoutes from './routes/worker'; */
+import workerRoutes from './routes/worker';
 
 const router = createBrowserRouter([
   {
     children: [
-      /* Ignore esta linha */
       { path: '', element: <Navigate to="admin/dashboard" /> },
       {
         path: '/admin',
         element: <AdminLayout />,
         children: adminRoutes,
       },
-      /* Adicione aqui o workerRoutes, seguindo o padrão utilizado para adminRoutes. */
+      {
+        path: '/worker',
+        element: <WorkerLayout />,
+        children: workerRoutes,
+      },
     ],
   },
 ]);
